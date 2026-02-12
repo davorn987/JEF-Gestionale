35bd948f48855c37c4534a90bf889d9c287aa3cf
davorn987 <davidenuzzi@yahoo.it>
2026-02-12 12:55:48 +0100

    Delete src/it/jeansandfriends/gestionale/model/ui directory

--- src/it/jeansandfriends/gestionale/model/ui/components/LookupText.java
+++
@@ -1,9 +0,0 @@
-package it.jeansandfriends.gestionale.ui.components;
-
-public final class LookupText {
-    private LookupText() {}
-
-    public static String safe(String s) {
-        return s == null ? "" : s;
-    }
-}
